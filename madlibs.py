6668c1b520c8ea98685f6835be33cc542272f3c1
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-05-16 16:23:45 -0400

    get user input and format madlibs string

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+# Get the user input for a verb, noun, and adjective
+verb = input('Please enter a verb: ')
+noun = input('Please enter a noun: ')
+adjective = input('Please enter an adjective: ')
+
+# Format the user's input into the madlibs statement
+print('I enjoy practice! I find it helps me to {} better. Without practice, my {} would probably be horrible. My craft is getting more {} evey single day!'.format(verb, noun, adjective))
